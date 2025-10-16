fb85e971f86426e0713335249ef19cbf381a16d8
indextorben <lehneketorben@gmail.com>
2025-10-16 15:39:23 +0200

    Bug fixxes

---
+++ TimerLiveActivity/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  TimerLiveActivity
+//
+//  Created by Torben Lehneke on 16.10.25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
